2fd33fef93e51039d0d38a6681f384b9bd556150
Roman Garanin <git@romangaranin.net>
2020-04-30 23:16:18 +0300

    feat: config write

--- file.go
+++ file.go
@@ -10,24 +10,9 @@ import (
 const configFilename = "overtimer.json"
 
 func getApp() (*App, error) {
-	jsonFile, err := os.Open(configFilename)
+	jsonFile, err := openConfigFile()
 	if err != nil {
-		if os.IsNotExist(err) {
-			fmt.Printf("%s does not exist, creating new file\n", configFilename)
-
-			jsonFile, err = os.Create(configFilename)
-			if err != nil {
-				return nil, err
-			}
-			_, err := jsonFile.WriteString("{}")
-			if err != nil {
-				return nil, err
-			}
-
-			return &App{}, nil
-		} else {
-			return nil, err
-		}
+		return nil, err
 	}
 	defer jsonFile.Close()
 
@@ -44,4 +29,54 @@ func getApp() (*App, error) {
 	}
 
 	return &app, nil
+}
+
+func openConfigFile() (*os.File, error) {
+	jsonFile, err := os.OpenFile(configFilename, os.O_RDWR, os.ModeExclusive)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("%s does not exist, creating new file\n", configFilename)
+
+			jsonFile, err = createNewConfigFile()
+			if err != nil {
+				return nil, err
+			}
+
+			return jsonFile, nil
+		} else {
+			return nil, err
+		}
+	}
+
+	return jsonFile, nil
+}
+
+func createNewConfigFile() (*os.File, error) {
+	jsonFile, err := os.Create(configFilename)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = jsonFile.WriteString("{}")
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonFile, nil
+}
+
+func (app *App) save() error {
+	jsonFile, err := openConfigFile()
+	if err != nil {
+		return err
+	}
+	defer jsonFile.Close()
+
+	bytes, err := json.MarshalIndent(app, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	_, err = jsonFile.Write(bytes)
+	return err
 }
