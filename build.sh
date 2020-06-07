de890010a7c5efbcf6e2e935b650cd4be6aa8306
Roman Garanin <git@romangaranin.net>
2020-06-07 17:05:09 +0300

    fix: build script updated with correct module

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-GOOS=linux GOARCH=amd64 go build -o overtimer_linux romangaranin.dev/overtimer && \
-GOOS=darwin GOARCH=amd64 go build -o overtimer_mac romangaranin.dev/overtimer && \
-GOOS=windows GOARCH=amd64 go build -o overtimer_win romangaranin.dev/overtimer
+GOOS=linux GOARCH=amd64 go build -o overtimer_linux github.com/EvilKhaosKat/overtimer && \
+GOOS=darwin GOARCH=amd64 go build -o overtimer_mac github.com/EvilKhaosKat/overtimer && \
+GOOS=windows GOARCH=amd64 go build -o overtimer_win github.com/EvilKhaosKat/overtimer
